92fd7c97c82bbc0920c8e8f01553f5aa78950741
Alvago-maestro <alvago-maestro@yandex.ru>
2024-08-28 23:18:00 +0600

    Add files via upload

---
+++ Peremennye.py
@@ -0,0 +1,8 @@
+kolichestvo_zadaniy = 12
+kolichestvo_chasov = 1.5
+nazvanie_kursa = 'Python'
+vremy_na_odno_zadanie = kolichestvo_chasov / kolichestvo_zadaniy
+print('Курс: ',nazvanie_kursa,
+      ', всего задач ', kolichestvo_zadaniy,
+      ', затрачено часов: ', kolichestvo_chasov,
+      ', среднее время выполнения ', vremy_na_odno_zadanie )
